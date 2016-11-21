e887ccbf7f4af3aa519dcdc5bf2ffff9b69fe62b
Chirag Bharadwaj <cb625@cornell.edu>
2016-11-21 01:35:22 -0500

    Fourth pass works: Cleaned Makefile, added lib support

---
+++ llvm/src/rtlib.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+/* Logs an integer on the command line screen.
+ *   - [i]: The integer to be printed. */
+void logop(int i) {
+  printf("computed: %i\n", i);
+}
---
+++ llvm/src/world.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+/* Simple "hello world" program. */
+int main(int argc, char* argv[]) {
+  printf("Hello world!\n");
+}
