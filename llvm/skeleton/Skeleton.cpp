1a6dba44a942c0d96619e43b0be9607c756c5364
Chirag Bharadwaj <cb625@cornell.edu>
2016-11-21 00:36:55 -0500

    Third pass works, instruction-replacing started

--- llvm/skeleton/Skeleton.cpp
+++ llvm/skeleton/Skeleton.cpp
@@ -2,7 +2,10 @@
 #include "llvm/IR/Function.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/IR/LegacyPassManager.h"
+#include "llvm/IR/InstrTypes.h"
 #include "llvm/Transforms/IPO/PassManagerBuilder.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/Transforms/Utils/BasicBlockUtils.h"
 using namespace llvm;
 
 namespace {
@@ -11,18 +14,27 @@ namespace {
     SkeletonPass() : FunctionPass(ID) {}
 
     virtual bool runOnFunction(Function &F) {
-      errs() << "In a function called " << F.getName() << "!\n";
-
-      errs() << "Function body:\n";
-      F.dump();
-
       for (auto &B : F) {
-        errs() << "Basic block:\n";
-        B.dump();
-
         for (auto &I : B) {
-          errs() << "Instruction: ";
-          I.dump();
+          if (auto *op = dyn_cast<BinaryOperator>(&I)) {
+            // Insert at the point where the instruction `op` appears.
+            IRBuilder<> builder(op);
+
+            // Make a multiply with the same operands as `op`.
+            Value *lhs = op->getOperand(0);
+            Value *rhs = op->getOperand(1);
+            Value *mul = builder.CreateMul(lhs, rhs);
+
+            // Everywhere the old instruction was used as an operand, use our
+            // new multiply instruction instead.
+            for (auto &U : op->uses()) {
+              User *user = U.getUser();  // A User is anything with operands.
+              user->setOperand(U.getOperandNo(), mul);
+            }
+
+            // We modified the code.
+            return true;
+          }
         }
       }
 
---
+++ llvm/src/add.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+/* Takes in an input number from the user and adds 2 to it. */
+int main(int argc, char* argv[]) {
+  int num;
+  scanf("%i", &num);
+  printf("%i\n", num + 2);
+  return 0;
+}
